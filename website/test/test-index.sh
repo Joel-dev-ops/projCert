d02f8e2cb2f10827795403ac3c594ccccf8fb378
joel1906 <dwaynej760@gmail.com>
2025-07-19 22:10:48 +0530

    Delete website/test directory

--- website/test/test-index.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running syntax tests..."
-php -l /var/www/html/index.php
-php -l /var/www/html/config.php
-php -l /var/www/html/functions.php
-
-echo "All syntax checks passed!"
